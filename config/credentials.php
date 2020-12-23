b5a6722410eba7f13303bae9f96ecbc1230e998c
techtenant <bnyilima@gmail.com>
2020-12-23 18:27:23 +0300

    Credentials setup

---
+++ config/credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+return[
+    'username'          => env('AS_USERNAME', 'sandbox'),
+    'api_key'           => env('AS_KEY', null),
+    'shortcode'         => env('AS_SHORTCODE', 'AFRICASTKNG'),
+    
+];
